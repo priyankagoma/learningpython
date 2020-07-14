9fb6cd2c472d12bcf8ff7b6d3ff15a83d494b75c
Priyanka <priyankagoma@outlook.com>
2020-07-14 16:58:31 +0100

    strings

--- src/string.py
+++ src/string.py
@@ -1,4 +1,10 @@
-print("Today is good day to learn python")
-print('Hello Nick', "you are looking good today", 'she can see you')
+# print("Today is good day to learn python")
+# print('Hello Nick', "you are looking good today", 'she can see you')
 
-print("Hello " + " World")
+# print("Hello " + " World")
+
+Greeting = "hello"
+Name = "priyanka"
+
+print(Greeting + ' ' + Name)
+print = input("please enter your name")
