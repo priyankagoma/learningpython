3a214a6b6f2d1dc76067bbdc1739dfaa7da73cef
Priyanka <priyankagoma@outlook.com>
2020-07-14 20:17:16 +0100

    how to run escape.

--- src/escapechar.py
+++ src/escapechar.py
@@ -1,11 +1,11 @@
-splitstring="Today my mood is not good\nand i do not want to talk\n to any one\or ill get mad on them"
+splitstring = "Today my mood is not good\nand i do not want to talk\n to any one\or ill get mad on them"
 print(splitstring)
 
 tabbedstring = "1\t 2\t 3\t 4\t 5\t 6\t"
 print(tabbedstring)
 
-print("My sister's dog is very cute but \"oh no he is not well today ")
-#or
-print('My sister\'s dog is very cute but \'oh no he is not well today')
+print('My sister\'s dog is very cute but \'oh no he\...\ uh are \'is not well today')
 
-print("'My siter's dog is very cute but \'oh no he is not well today'")
+print("""My siter's dog is very cute but \'oh no he \...\ uh are\'is not well today""")
+
+print('My sister\'s dog is very cute but \'oh no he \...\ uh are\'is not well today')
