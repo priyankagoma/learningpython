af4a21b77b358d079393c9c4a256b467061d69b8
Priyanka <priyankagoma@outlook.com>
2020-07-15 16:47:44 +0100

    learning anothersplitstring.

--- src/escapechar.py
+++ src/escapechar.py
@@ -1,13 +1,16 @@
-splitstring = "Today my mood is not good\nand i do not want to talk\nto any one\or ill get mad on them"
-print(splitstring)
+#splitstring = "Today my mood is not good\nand i do not want to talk\nto any one\or ill get mad on them"
+#print(splitstring)
+#tabbedstring = "1\t 2\t 3\t 4\t 5\t 6\t"
+#print(tabbedstring)
+#print('My sister\'s dog is very cute but \'oh no he\...\ uh are \'is not well today')
+#print("""My siter's dog is very cute but \'oh no he \...\ uh are\'is not well today""")
+#print('My sister\'s dog is very cute but \'oh no he \...\ uh are\'is not well today')
 
-tabbedstring = "1\t 2\t 3\t 4\t 5\t 6\t"
-print(tabbedstring)
+anotherSplitString = """This is not
+my fault
+but i have to
+deal with 
+this
+any how"""
 
-print('My sister\'s dog is very cute but \'oh no he\...\ uh are \'is not well today')
-
-print("""My siter's dog is very cute but \'oh no he \...\ uh are\'is not well today""")
-
-print('My sister\'s dog is very cute but \'oh no he \...\ uh are\'is not well today')
-
-how to use splitstring, tabbed string and escape.
+print(anotherSplitString)
