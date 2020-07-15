69a6830fe3a0802a9273fafd69f4789ae7cb41f4
Priyanka <priyankagoma@outlook.com>
2020-07-15 19:09:05 +0100

    learning anothersplitstring.

--- src/escapechar.py
+++ src/escapechar.py
@@ -1,16 +1,14 @@
-#splitstring = "Today my mood is not good\nand i do not want to talk\nto any one\or ill get mad on them"
-#print(splitstring)
-#tabbedstring = "1\t 2\t 3\t 4\t 5\t 6\t"
-#print(tabbedstring)
-#print('My sister\'s dog is very cute but \'oh no he\...\ uh are \'is not well today')
-#print("""My siter's dog is very cute but \'oh no he \...\ uh are\'is not well today""")
-#print('My sister\'s dog is very cute but \'oh no he \...\ uh are\'is not well today')
+splitstring = "Today my mood is not good   \nand i do not want to talk   \nto any one   \nor ill get mad on them"
+print(splitstring)
+tabbedstring = "1  \t  2\t  3\t  4\t  5\t  6\t"
+print(tabbedstring)
 
-anotherSplitString = """This is not
-my fault
-but i have to
-deal with 
-this
-any how"""
+print('My sister\'s dog is very cute but \'oh no he\...\ uh are \'is not well today')
+print("""My siter's dog is very cute but \'oh no he \...\ uh are\'is not well today""")
+print('My sister\'s dog is very cute but \'oh \
+no he \...\ uh are\'is not well today')
 
-print(anotherSplitString)
+print(""" This is not \
+the way \
+to talk \
+to some one""")
