e873e54db5399b01331f268e89dbe4a959cc4b04
Priyanka <priyankagoma@outlook.com>
2020-08-06 15:53:45 +0100

    uses of slashes tabbedstring, splitstring and escape Apostrophes.

--- src/escapechar.py
+++ src/escapechar.py
@@ -1,10 +1,23 @@
+# How to create tabbed and splitstring.
+
 splitstring = "Today my mood is not good   \nand i do not want to talk   \nto any one   \nor ill get mad on them"
 print(splitstring)
 tabbedstring = "1  \t  2\t  3\t  4\t  5\t  6\t"
 print(tabbedstring)
 
+#course exercise
+print("Number 1\t the horse\nNumber 2\t the chestnut")
+print()
+print(""" Number 1     the horse 
+ Number 2     the chestnut""")
+
+# Also use slashes to escape single quotes or Apostrophes.
+
 print('My sister\'s dog is very cute but \'oh no he\... uh are \is not well today')
 print("""My siter's dog is very cute but 'oh no he \...\ uh are' is not well today""")
+
+#In this book we will learn that how with the help of slashes (/) we can make or break a string.
+
 print('My sister\'s dog is very cute but \'oh \
 no he \...\ uh are\'is not well today')
 
