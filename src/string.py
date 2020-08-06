f14c102b3c6cc0390ddc969de07eec5d90df04e7
Priyanka <priyankagoma@outlook.com>
2020-08-06 16:51:22 +0100

    string, variables and input function in string.

--- src/string.py
+++ src/string.py
@@ -1,14 +1,16 @@
+# How to use double and single quotes in string.
+
 print("Today is good day to learn python")
 print()
 print('Hello Nick', "you are looking good today", 'she can see you')
-print()
-print("Hello " + " World")
-print()
-Greeting = "hello"
-print()
-print(Greeting)
-print("Number 1\t the horse\nNumber 2\t the chestnut")
-print()
-print(""" Number 1     the horse 
- Number 2     the chestnut""")
 
+# string concatenation.
+
+print("Hello " + " World")
+
+# variables in string and how to use input function.
+
+Greeting = "hello"
+Name = input("Please enter your name")
+care = input("Question")
+print(Greeting + " " + Name + " " + care)
