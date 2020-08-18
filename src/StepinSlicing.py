dae08e4652595757eaa9262bbc08b555aefe6a11
Priyanka <priyankagoma@outlook.com>
2020-08-18 19:08:36 +0100

    step in slicing and backward step in slicing.

--- src/StepinSlicing.py
+++ src/StepinSlicing.py
@@ -1,18 +1,30 @@
 # Using a step in slice, it can be step 2,3,4,5,etc...
 Numbers = "12345678901234567890"
 Ex: print(Numbers[0:6:2])
-# so we can see the slice starts at index position and it extends up to (not included) position 6.
-# so we are stepping through the sequence in steps of 2.
+# so we can see the slice starts at index position 0 and it extends up to (not included) position 6.
+# now we are stepping through the sequence in steps of 2.
 
 Numbers = "12345678901234567890"
+Seperators = Numbers[1:20:1]
+print(Seperators)
 Seperators = Numbers[1::3]
 print(Seperators)
+Seperators = Numbers[:18:2]
+print(Seperators)
+          #1234567890123456789012345678901
+Letters = "a b c d e f g h i j k l m n o p"
+
+print(Letters[:20:1])
+print(Letters[::2])
+print(Letters[1:21:2])
 
 # Step in Slicing Backward.
-test = "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20"
-      # 20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1-
-print(test[18:1:-1])
+##########################
+test = "1 2 3 4 5 6 7 8 9 0"
+# 9876543219876543210-
+print(test[19:0:-2])
+print(test[19::-1])
 
 Letters = "abcdefghijklmnopqrstuvwxyz"
-        # 765432109876543210987654321-
-print(test[18:2:-2])
+         # 54321098765432109876543210-
+print(Letters[25:0:-2])
