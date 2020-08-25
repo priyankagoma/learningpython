4a0ce001843bed1babe9e44a905beaa7a87fdeb9
Priyanka <priyankagoma@outlook.com>
2020-08-26 00:37:41 +0100

    Added new file in repo.

---
+++ src/Sequnce_operators.py
@@ -0,0 +1,22 @@
+string1 = "Priyanka "
+string2 = "Nishtha "
+string3 = "Bobby "
+string4 = "Tavishi "
+string5 = "Kanika "
+
+print(string1 + string4 + string5)
+print("Priyanka " "Tavishi " "Kanika ")
+
+print("Hello " * 5)
+print("Hello " * 4)
+print("Hello " + 4 * "5")
+print("Hello " * (4 + 5))
+print()
+Butterfly = "Red Blue Green Purple"
+print("Red" in Butterfly)
+print("Blue" in Butterfly)
+print("Orange" in Butterfly)
+print()
+today = "Tuesday"
+print("day" in today)
+print("Fri" in today)
